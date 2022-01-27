bb9e8cff43397394f7c11b225115fe4fa73c0dfe
Werner Luiz Gottschalt <wgottschalt.projects@gmail.com>
2022-01-26 21:37:20 -0300

    trash

--- tmp/installers/make_directories.sh
+++
@@ -1,9 +0,0 @@
-#!/bash/bin
-
-# Directory
-mkdir ~/Documentos/Databases
-mkdir ~/Documentos/Projects
-mkdir ~/Documentos/Projects/JCSx
-mkdir ~/Documentos/Projects/MelhorEnvio
-mkdir ~/Documentos/Projects/Personal
-mkdir ~/Documentos/Projects/Studies
